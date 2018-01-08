d94b36b0056c52ddf4a9f0fc93ff0987a00f2119
sanabr_l <sanabr_l@720ce22c-1725-4da0-b027-f049bdf0dce9>
2018-01-08 17:54:06 +0000

    count_number_of_functions

--- my_norme.php
+++ my_norme.php
@@ -10,9 +10,28 @@
 //
 
 //function which opens the file. Variable is a string returned by the function
+//function longer than 25 lines
+//wrong header
+//number of function per file
+//space at the end of a line
+//missing space after keyword
+//carriot return twice
+//line longer than 80 characters
+//more than 4 parameters for a function
+//where includes appear
+//declare and define a variable on the same line
+//Define in .c file
+//carriot return between two functions
+//tabulation when a variable is declared
+//carriot return when variables defined
+//display colors for better readability
 
 require_once('open_file.php');
 require_once('how_long_func.php');
+require_once('how_many_func.php');
+require_once('space_after_keyword.php');
 
 $file = open_file($argv[1]);
 print how_long_func($file);
+print how_many_func($file);
+space_after_keyword($file);
