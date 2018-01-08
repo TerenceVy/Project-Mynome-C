9cd1ab90d73bd20a5a0f515d34182f3acad71c41
sanabr_l <sanabr_l@720ce22c-1725-4da0-b027-f049bdf0dce9>
2018-01-08 15:01:50 +0000

    string

--- my_norme.php
+++ my_norme.php
@@ -1,12 +1,18 @@
-#!/usr/bin/env php
 <?php
-// my_norme.php for main in 
-// 
+#!/usr/bin/env php
+// my_norme.php for main in
+//
 // Made by SANABRIA Luis-Edouard
 // Login   <sanabr_l@etna-alternance.net>
-// 
+//
 // Started on  Mon Jan  8 11:35:18 2018 SANABRIA Luis-Edouard
 // Last update Mon Jan  8 11:36:18 2018 SANABRIA Luis-Edouard
 //
 
-print($argv[1]);
+//function which opens the file. Variable is a string returned by the function
+
+require_once('open_file.php');
+require_once('how_long_func.php');
+
+$file = open_file($argv[1]);
+how_long_func($file);
