6d5841e6aa20289006e571011a841680b0663f01
sanabr_l <sanabr_l@720ce22c-1725-4da0-b027-f049bdf0dce9>
2018-01-08 10:47:08 +0000

    read_arg

---
+++ my_norme.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+// my_norme.php for main in 
+// 
+// Made by SANABRIA Luis-Edouard
+// Login   <sanabr_l@etna-alternance.net>
+// 
+// Started on  Mon Jan  8 11:35:18 2018 SANABRIA Luis-Edouard
+// Last update Mon Jan  8 11:36:18 2018 SANABRIA Luis-Edouard
+//
+
+print($argv[1]);
