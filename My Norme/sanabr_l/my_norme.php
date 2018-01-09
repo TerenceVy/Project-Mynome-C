2796331b7b8454ac5bc07f738435835938760a4a
sanabr_l <sanabr_l@720ce22c-1725-4da0-b027-f049bdf0dce9>
2018-01-09 13:31:12 +0000

    beyondEighty

--- My Norme/sanabr_l/my_norme.php
+++ My Norme/sanabr_l/my_norme.php
@@ -31,19 +31,28 @@ function open_file_str($file_to_open) {
   }
 }
 
+$file_arr = open_file_arr($argv[1]);
+$file_str = open_file_str($argv[1]);
+
 //function longer than 25 lines
 require_once('how_long_func.php');
+print how_long_func($file_arr);
 
 //wrong header
 //number of function per file
 require_once('how_many_func.php');
+print how_many_func($file_arr);
 
 //space at the end of a line
 //missing space after keyword
 require_once('space_after_keyword.php');
+print space_after_keyword($file_str);
 
 //carriot return twice
 //line longer than 80 characters
+require_once('beyond_eighty.php');
+print beyond_eighty($file_arr);
+
 //more than 4 parameters for a function
 //where includes appear
 //declare and define a variable on the same line
@@ -52,11 +61,3 @@ require_once('space_after_keyword.php');
 //tabulation when a variable is declared
 //carriot return when variables defined
 //display colors for better readability
-
-
-$file_arr = open_file_arr($argv[1]);
-$file_str = open_file_str($argv[1]);
-
-print how_long_func($file_arr);
-print how_many_func($file_arr);
-print space_after_keyword($file_str);
