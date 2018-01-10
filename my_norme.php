463124dcd9f58f7a75c053af03ce4c0d8ee884b8
sanabr_l <sanabr_l@720ce22c-1725-4da0-b027-f049bdf0dce9>
2018-01-10 15:57:37 +0000

    total

--- my_norme.php
+++ my_norme.php
@@ -1,73 +1,252 @@
 <?php
-#!/usr/bin/env php
-// my_norme.php for main in
-//
-// Made by SANABRIA Luis-Edouard
-// Login   <sanabr_l@etna-alternance.net>
-//
-// Started on  Mon Jan  8 11:35:18 2018 SANABRIA Luis-Edouard
-// Last update Mon Jan  8 11:36:18 2018 SANABRIA Luis-Edouard
-//
-
-//function to open the file. Returns an array, each element being a line of the file
-function open_file_arr($file_to_open)
-{
-  $file = @file($file_to_open);
-
-  if ($file == FALSE)
-    return "Oops ! Something wrong happened : no file found.\n";
-  else
-    return $file;
-}
-
-//function to open the file. Returns a string
-function open_file_str($file_to_open)
-{
-  $file = @file_get_contents($file_to_open);
-
-  if ($file == FALSE)
-    return "Oops ! Something wrong happened : no file found.\n";
-  else
-    return $file;
-}
-
-$file_arr = open_file_arr($argv[1]);
-$file_str = open_file_str($argv[1]);
-//var_dump($file_str);
+$compt_faute = 0;
 
 //function longer than 25 lines
-require_once('how_long_func.php');
-print "Nombre de ligne en trop pour une fonction => ".how_long_func($file_arr)."\n";
+function how_long_func($file)
+{
+	global $compt_faute;
+  $i = 0;
+  $j = 0;
+  $k = 0;
+  $mistake = 0;
+  $begin_func = '/^(void)[\ ]+[a-z\_]+[\(][a-z]+[\ ][a-z\*]+(\))/';
 
-//wrong header
-//number of function per file
-require_once('how_many_func.php');
-print "Nombre fonction en trop => ".how_many_func($file_arr)."\n";
+  while ($i < sizeof($file))
+  {
+    if (preg_match($begin_func, $file[$i]))
+      $j = $i;
+    if (preg_match('/(\{)/', $file[$i]))
+      ++$k;
+    if (preg_match('/(\})/', $file[$i]))
+      --$k;
+    if ($k == 0)
+    {
+      if ($i - $j > 27)
+      {
+        $mistake = $mistake + $i - $j - 27;
+        $line = $i - $mistake + 1;
+        print "Erreur a partir de la ligne ".$line.", trop de ligne.\n";
+      }
+      $j = $i;
+    }
+    ++$i;
+  }
+	$compt_faute += $mistake;
+  return $mistake;
+}
 
-//space at the end of a line
-//missing space after keyword
-require_once('space_after_keyword.php');
-print "Espace apres mot cle => ".space_after_keyword($file_arr)."\n";
+//Number of function in a file
+function how_many_func($file)
+{
+	global $compt_faute;
+  $type = '(int|char|void|float|double|long double)';
+  $decl_fun1 = '/^'.$type.'[\ ]+[\*]*[a-z\_]+(\()'.$type.'+[\ ][a-z\*]+(\))$/';
+  $decl_fun2 = '/^'.$type.'[\ ]+[\*]*[a-z\_]+(\(\))$/';
+  $decl_main = '/^'.$type.'[\ ]+[\*]*(main)$/';
+  $mistake = 0;
 
-//carriot return twice
-//line longer than 80 characters
-require_once('beyond_eighty.php');
-print "Plus de 80 caracteres sur une ligne => ".beyond_eighty($file_arr)."\n";
+  for ($i = 0; isset($file[$i]); ++$i)
+  {
+    if (preg_match($decl_fun1, $file[$i]))
+      if (preg_match($decl_main, $file[$i]) == FALSE)
+      {
+        ++$mistake;
+        if ($mistake > 5)
+          print "Erreur a la ligne ".($i + 1).", une fonction de trop.\n";
+      }
+    else if (preg_match($decl_fun2, $file[$i]))
+      if (preg_match($decl_main, $file[$i]) == FALSE)
+      {
+        ++$mistake;
+        if ($mistake > 5)
+          print "Erreur a la ligne ".($i + 1).", une fonction de trop.\n";
+      }
+  }
+  if ($mistake > 5) {
+		$compt_faute += $mistake;
+    return $mistake - 5;
+	}
+  else
+    return 0;
+}
 
-//more than 4 parameters for a function
-//where includes appear
-require_once('order_incl.php');
-print "Des includes mal places => ".order_incl($file_arr)."\n";
+//Space at the end of a line
+function white_space_on_line($line)
+{
+	global $compt_faute;
+	$faute = 0;
+	foreach ($line as $value)
+	{
+		if (preg_match('/ $/',$value))
+			++$faute;
+	}
+	echo "Espace en fin de ligne : " . $faute . " fautes\n";
+	$compt_faute += $faute;
+}
 
-//declare and define a variable on the same line
-//Define in source file
-require_once('define_in_source.php');
-print "Des defines dans le code source => ".define_in_source_file($file_arr)."\n";
+//Missing space after a keyword
+function space_after_keyword($file)
+{
+	global $compt_faute;
+  $keyword = array("auto", "break", "case", "char", "const", "continue",
+  "default", "double", "else", "enum", "extern", "float", "for", "if", "int",
+  "long", "register", "return", "short", "signed", "static", "struct",
+  "typedef", "union", "unsigned", "void", "volatile", "while");
+  $i = 0;
+  $j = 0;
+  $mistake = 0;
 
-//carriot return between two functions
-//tabulation when a variable is declared
-require_once("tab_in_declaration.php");
-print "Manque de tabulation dans les variables => ".tab_in_declaration($file_arr)."\n";
+  while ($i < sizeof($file))
+  {
+    while ($j < sizeof($keyword))
+    {
+      if (preg_match("/\b(".$keyword[$j].")[^\ ]/", $file[$i]))
+      {
+        ++$mistake;
+        print "Erreur a la ligne ".$i.", espace manquant apres un mot-cle.\n";
+      }
+      ++$j;
+    }
+    $j = 0;
+    ++$i;
+  }
+	$compt_faute += $mistake;
+  return $mistake;
+}
 
-//carriot return when variables defined
-//display colors for better readability
+//Carriot return twice
+function double_return($line)
+{
+	global $compt_faute;
+	$faute = 0;
+	foreach ($line as $value)
+	{
+		if (preg_match('/^\n/', $value))
+			++$faute;
+	}
+		echo "Double retour à la ligne : " . $faute . "fautes\n";
+		$compt_faute += $faute;
+}
+
+//Line longer than 80 characters
+function beyond_eighty($file_arr)
+{
+	global $compt_faute;
+  $mistake = 0;
+
+  for ($i = 0; $i < sizeof($file_arr); ++$i)
+  {
+    if (preg_match('/^(\/\/)/', $file_arr[$i]) == FALSE)
+    {
+      if (strlen($file_arr[$i]) > 80)
+      {
+        print "Erreur a la ligne ".$i.", trop de caracteres pour la ligne.\n";
+        $mistake += strlen($file_arr[$i]) - 80;
+      }
+    }
+    ++$i;
+  }
+	$compt_faute += $mistake;
+  return $mistake;
+}
+
+//Check where includes appear
+function order_incl($file)
+{
+	global $compt_faute;
+  $i = 0;
+  $j = 0;
+  $mistake = 0;
+  $begin_func = '/^(void)[\ ]+[a-z\_]+[\(][a-z]+[\ ][a-z\*]+(\))/';
+  $check_incl = '/^(#include)/';
+
+  while (isset($file[$i]))
+  {
+    if (preg_match($begin_func, $file[$i]))
+      ++$j;
+    if ($j !== 0)
+      if (preg_match($check_incl, $file[$i]))
+        ++$mistake;
+    ++$i;
+  }
+	$compt_faute += $mistake;
+  return $mistake;
+}
+
+//Declare and define on the same line
+function declar_affect($line)
+{
+	global $compt_faute;
+	$faute = 0;
+	foreach ($line as $value)
+	{
+		if(preg_match('/int|tab|float|char|doublefloat|long|signed|unsigned|short|double/', $value))
+		{
+			if(preg_match('/=/',$value))
+				++$faute;
+		}
+	}
+	echo "Déclaration + Affectation même ligne : " . $faute . "fautes\n";
+	$compt_faute += $faute;
+}
+
+//Define in a source file
+function define_in_source_file($file)
+{
+	global $compt_faute;
+  $i = 0;
+  $mistake = 0;
+  $check_define = '/^(#define )/';
+
+  while (isset($file[$i]))
+  {
+    if (preg_match($check_define, $file[$i]))
+      ++$mistake;
+    ++$i;
+  }
+	$compt_faute += $mistake;
+  return $mistake;
+}
+
+//Carriot return between two functions
+function two_return_func($line)
+{
+	global $compt_faute;
+	$faute = 0;
+	foreach ($line as $value)
+	{
+		if (preg_match('/}/', $value))
+		{
+			++$value;
+			if (preg_match('/(^void|int|char)/', $value) )
+				++$faute;
+		}
+	}
+	echo "Retour à la ligne entre 2 fonctions : " . $faute . " fautes\n";
+	$compt_faute += $faute;
+}
+
+$dir = $argv[1];
+$files = scandir($dir); // pour tous les fichiers
+
+for ($i = 0; $i < count($files); $i++)
+{
+	$compteur = 0;
+	if (preg_match('/(\.c|\.h)$/', $files[$i]))
+	{
+		$line = file($files[$i]);
+		$last_line = count($line);
+		how_long_func($line);
+		how_many_func($line);
+		white_space_on_line($line);
+		space_after_keyword($line);
+		double_return($line);
+		beyond_eighty($line);
+		order_incl($line);
+		declar_affect($line);
+		define_in_source_file($line);
+		two_return_func($line);
+		print $compt_faute."\n";
+		$compt_faut = 0;
+	}
+}
